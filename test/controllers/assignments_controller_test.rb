ecd6cee9eb4fe6abeb228a2b0a6f3ef86c6d2eaa
Øyvind Guttvik Årnes <oyvind.arnes@bouvet.no>
2024-12-27 12:40:58 +0100

    fixed controller tests

--- test/controllers/assignments_controller_test.rb
+++ test/controllers/assignments_controller_test.rb
@@ -5,11 +5,6 @@ class AssignmentsControllerTest < ActionDispatch::IntegrationTest
     @assignment = assignments(:one)
   end
 
-  test "should get index" do
-    get assignments_url
-    assert_response :success
-  end
-
   test "should get new" do
     get new_assignment_url
     assert_response :success
@@ -17,32 +12,78 @@ class AssignmentsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create assignment" do
     assert_difference("Assignment.count") do
-      post assignments_url, params: { assignment: { private_task_code: @assignment.private_task_code, public_task_code: @assignment.public_task_code } }
+      post assignments_url, params: { assignment: { name: "New assignment", wrong_translations: "0", words_attributes: [ { original_text: "Original text", translated_text: "Translated text" } ] } }
     end
 
-    assert_redirected_to assignment_url(Assignment.last)
+    assert_redirected_to assignment_summary_path(Assignment.last.private_task_code)
   end
 
-  test "should show assignment" do
-    get assignment_url(@assignment)
+  test "should show assignment summary" do
+    get assignment_summary_url(@assignment.private_task_code)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_assignment_url(@assignment)
+    get edit_assignment_url(@assignment.private_task_code)
     assert_response :success
   end
 
   test "should update assignment" do
-    patch assignment_url(@assignment), params: { assignment: { private_task_code: @assignment.private_task_code, public_task_code: @assignment.public_task_code } }
-    assert_redirected_to assignment_url(@assignment)
+    patch update_assignment_url(@assignment.private_task_code), params: { assignment: { name: "Updated assignment", wrong_translations: "0", words_attributes: [ { original_text: "New Original text", translated_text: "New Translated text" } ] } }
+    assert_redirected_to assignment_summary_path(Assignment.last.private_task_code)
   end
 
   test "should destroy assignment" do
     assert_difference("Assignment.count", -1) do
-      delete assignment_url(@assignment)
+      delete destroy_assignment_url(@assignment.private_task_code)
     end
 
-    assert_redirected_to assignments_url
+    assert_redirected_to root_url
+  end
+
+  test "should create run from welcome" do
+    post new_run_from_welcome_url, params: { public_task_code: @assignment.public_task_code }
+    assert_redirected_to new_run_path(public_task_code: @assignment.public_task_code)
+  end
+
+  test "should create run" do
+    post create_run_url(@assignment.public_task_code), params: { selected_levels: [ "1", "2", "3" ] }
+    assert_redirected_to level_one_url(@assignment.public_task_code)
+  end
+
+  test "should get level one" do
+    post create_run_url(@assignment.public_task_code), params: { selected_levels: [ "1" ] }
+    get level_one_url(@assignment.public_task_code)
+    assert_response :success
+  end
+
+  test "should post level one answer" do
+    post create_run_url(@assignment.public_task_code), params: { selected_levels: [ "1" ] }
+    post level_one_answer_url(@assignment.public_task_code), params: { guess: "some guess" }
+    assert_response :success
+  end
+
+  test "should get level others" do
+    post create_run_url(@assignment.public_task_code), params: { selected_levels: [ "2" ] }
+    get level_others_url(@assignment.public_task_code)
+    assert_response :success
+  end
+
+  test "should post level others answer" do
+    post create_run_url(@assignment.public_task_code), params: { selected_levels: [ "2" ] }
+    post level_others_answer_url(@assignment.public_task_code), params: { guess: "some guess" }
+    assert_response :success
+  end
+
+  test "should get next step" do
+    post create_run_url(@assignment.public_task_code), params: { selected_levels: [ "1" ] }
+    post level_one_answer_url(@assignment.public_task_code), params: { guess: "some guess" }
+    get next_step_url(@assignment.public_task_code)
+    assert_redirected_to completed_url(@assignment.public_task_code)
+  end
+
+  test "should add word" do
+    get add_word_new_assignments_url(format: :turbo_stream)
+    assert_response :success
   end
 end
