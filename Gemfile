da00b7b438048c08ebb19f79d5f28ad45b4eca62
Øyvind Guttvik Årnes <oyvind.arnes@hey.com>
2025-01-11 19:14:51 +0100

    Added qr code for public assignment link

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "rails-i18n"
 gem "stackprof"
 gem "sentry-ruby"
 gem "sentry-rails"
+gem "rqrcode", "~> 2.0"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -66,6 +66,8 @@ class AssignmentsController < ApplicationController
   end
 
   def summary
+    qr_code = RQRCode::QRCode.new(new_run_url(@assignment.public_task_code)).as_png
+    @image_data = Base64.strict_encode64(qr_code.to_blob)
   end
 
   def edit
