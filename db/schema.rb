a557e6a111dc972ab5811285e2fe32d7835305b7
Øyvind Guttvik Årnes <oyvind.arnes@hey.com>
2025-03-12 18:52:55 +0100

    upgrading ruby and rails, work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_27_164753) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_12_174715) do
   create_table "assignments", force: :cascade do |t|
     t.string "public_task_code"
     t.string "private_task_code"
